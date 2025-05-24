90b04c1bd3b3c7a19b985145b3c482fd359f1f31
chihulloli123 <chihulloli123@gmail.com>
2025-05-23 19:15:01 -0500

    Update app.py
    Damn Ports lolz

--- backend/app.py
+++ backend/app.py
@@ -50,4 +50,4 @@ def get_tree():
     return jsonify(tree)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
